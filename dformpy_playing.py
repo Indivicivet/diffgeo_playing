c93e589154bd2c1b82db96506ad6a34661263ffe
indi <indivicivet@gmail.com>
2023-09-20 23:45:06 +0100

    plot 1form exterior derivative -- fewer sample points are better!

--- dformpy_playing.py
+++ dformpy_playing.py
@@ -2,16 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import dformpy
 
-SAMPLE_POINTS = 100
+SAMPLE_POINTS = 10
 
 xx, yy = np.meshgrid(
     np.linspace(-1, 1, SAMPLE_POINTS),
     np.linspace(-1, 1, SAMPLE_POINTS),
 )
 radial_0form = dformpy.form_0(
-    xx, yy, (xx ** 2 + yy ** 2) ** 0.5
+    xx,
+    yy,
+    (xx ** 2 + yy ** 2) ** 0.5,
+    form_0_eqn="(x**2 + y ** 2) ** 0.5",
 )
 
 plt.figure(figsize=(8, 8))
-radial_0form.plot(plt.gca())
+radial_0form.ext_d().plot(plt.gca())
 plt.show()
