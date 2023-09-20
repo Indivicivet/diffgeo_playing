3942e2bcb532f6225ce4ca085868e52fbfa99839
indi <indivicivet@gmail.com>
2023-09-21 00:33:43 +0100

    update pendulum - plot hamiltonian and... its flow? maybe?

--- dformpy_pendulum.py
+++ dformpy_pendulum.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import dformpy
 
-SAMPLE_POINTS = 50
-MAX_MOMENTUM = 12
+SAMPLE_POINTS = 40
+MAX_MOMENTUM = 20
 
 MASS = 1
 LENGTH = 1
@@ -13,7 +13,7 @@ thetatheta, pp = np.meshgrid(
     np.linspace(-np.pi, np.pi, SAMPLE_POINTS),
     np.linspace(-MAX_MOMENTUM, MAX_MOMENTUM, SAMPLE_POINTS),
 )
-radial_0form = dformpy.form_0(
+hamiltonian = dformpy.form_0(
     thetatheta,
     pp,
     (
@@ -21,11 +21,14 @@ radial_0form = dformpy.form_0(
         + MASS * GRAVITY * LENGTH * (1 - np.cos(thetatheta))
     ),
     form_0_eqn=(
-        f"x**2 / {2 * MASS * LENGTH}"
-        f" + {MASS * GRAVITY * LENGTH} * (1 - cos(y))"
+        f"y**2 / {2 * MASS * LENGTH}"
+        f" + {MASS * GRAVITY * LENGTH} * (1 - cos(x))"
     ),
 )
 
-plt.figure(figsize=(8, 8))
-radial_0form.plot(plt.gca())
+_, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 8))
+hamiltonian.plot(ax1)
+hamiltonian.ext_d().contravariant(
+    g=np.array([[0, 1], [-1, 0]]).reshape(2, 2, 1)
+).plot(ax2)
 plt.show()
