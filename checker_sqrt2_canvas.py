3efcaa91db3552b5a3b1958db3da0564fc9b6a9f
indi <indivicivet@gmail.com>
2024-10-24 22:45:56 +0100

    wip not working checker canvas thing

---
+++ checker_sqrt2_canvas.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 10
+SPACING_CONSTANT = 0.05
+
+
+def force_between(pt, other, is_same) -> np.ndarray:
+    """
+    force d/x^2 - 1/x
+    """
+    delta = pt - other
+    dist = (delta**2).sum() ** 0.5
+    if dist == 0:
+        print(pt, other, is_same, "dist is 0???")
+        return np.array([0, 0])
+    return (delta / dist) * ([2**0.5, 1][is_same] * SPACING_CONSTANT / dist - 1) / dist
+
+
+def evolve(aa, bb, delta_t):
+    aa = aa.copy()
+    bb = aa.copy()
+    for i, pt0 in enumerate(aa):
+        aa[i] += delta_t * (
+            sum(
+                force_between(pt0, pt1, is_same=True)
+                for j, pt1 in enumerate(aa)
+                if i != j
+            )
+            + sum(force_between(pt0, pt1, is_same=False) for pt1 in bb)
+        )
+    for i, pt0 in enumerate(bb):
+        bb[i] += delta_t * (
+            sum(
+                force_between(pt0, pt1, is_same=True)
+                for j, pt1 in enumerate(bb)
+                if i != j
+            )
+            + sum(force_between(pt0, pt1, is_same=False) for pt1 in aa)
+        )
+    print(aa, bb)
+    return aa, bb
+
+
+if __name__ == "__main__":
+    rng = np.random.default_rng(seed=23405)
+
+    pts_a = rng.uniform(low=-1, high=1, size=[N, 2])
+    pts_b = rng.uniform(low=-1, high=1, size=[N, 2])
+
+    plt.scatter(*pts_a.T, label="pts a 0")
+    plt.scatter(*pts_b.T, label="pts b 0")
+
+    for iter in range(1, 2):
+        pts_a, pts_b = evolve(pts_a, pts_b, delta_t=0.1)
+        plt.scatter(*pts_a.T, label=f"pts a {iter}")
+        plt.scatter(*pts_b.T, label=f"pts b {iter}")
+
+    plt.legend()
+    plt.show()
