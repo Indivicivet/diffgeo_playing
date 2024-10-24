0c31691998581a05936868e3b209b2062b64f517
indi <indivicivet@gmail.com>
2024-10-24 22:55:21 +0100

    make code more sensible, but not quite - accidentally makes pokeballs

--- checker_sqrt2_canvas.py
+++ checker_sqrt2_canvas.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 10
-SPACING_CONSTANT = 0.05
+N = 20
+SPACING_CONSTANT = 1.5 * (2 * N) ** -0.5
+
+FORCE_CLIP = 3
 
 
 def force_between(pt, other, is_same) -> np.ndarray:
@@ -12,9 +14,11 @@ def force_between(pt, other, is_same) -> np.ndarray:
     delta = pt - other
     dist = (delta**2).sum() ** 0.5
     if dist == 0:
-        print(pt, other, is_same, "dist is 0???")
+        print(pt, other, is_same, "dist is 0???")  # todo :: exception?
         return np.array([0, 0])
-    return (delta / dist) * ([2**0.5, 1][is_same] * SPACING_CONSTANT / dist - 1) / dist
+    force_weight = ([2**0.5, 1][is_same] * SPACING_CONSTANT / dist - 1) / dist
+    # print(force_weight)
+    return (delta / dist) * np.clip(force_weight, -FORCE_CLIP, FORCE_CLIP)
 
 
 def evolve(aa, bb, delta_t):
@@ -38,7 +42,6 @@ def evolve(aa, bb, delta_t):
             )
             + sum(force_between(pt0, pt1, is_same=False) for pt1 in aa)
         )
-    print(aa, bb)
     return aa, bb
 
 
@@ -51,10 +54,13 @@ if __name__ == "__main__":
     plt.scatter(*pts_a.T, label="pts a 0")
     plt.scatter(*pts_b.T, label="pts b 0")
 
-    for iter in range(1, 2):
-        pts_a, pts_b = evolve(pts_a, pts_b, delta_t=0.1)
-        plt.scatter(*pts_a.T, label=f"pts a {iter}")
-        plt.scatter(*pts_b.T, label=f"pts b {iter}")
+    PLOT_ITERS = [1, 5, 10, 20, 40]
+
+    for iter in range(1, max(PLOT_ITERS) + 1):
+        pts_a, pts_b = evolve(pts_a, pts_b, delta_t=0.01)
+        if iter in PLOT_ITERS:
+            plt.scatter(*pts_a.T, label=f"pts a {iter}")
+            plt.scatter(*pts_b.T, label=f"pts b {iter}")
 
     plt.legend()
     plt.show()
